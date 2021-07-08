527a9b75883bf56c336402df33c3c464072e0e91
Chuck Sebian-Lander <csebianlander@gmail.com>
2021-07-08 10:53:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,14 @@ process.on('unhandledRejection', error => {
 });
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://chuck:tangabutts@cluster0.ywpal.mongodb.net/cobpe.cobpe_auth?retryWrites=true&w=majority", { useNewUrlParser: true} );
+await mongoose.connect('mongodb://chuck:tangabutts@cluster0.ywpal.mongodb.net/cobpe.cobpe_auth?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+// mongoose.connect("mongodb://chuck:tangabutts@cluster0.ywpal.mongodb.net/cobpe.cobpe_auth?retryWrites=true&w=majority", { useNewUrlParser: true} );
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
