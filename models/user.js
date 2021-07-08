10b330066c5f3a048097917847bb663b7392a232
Chuck Sebian-Lander <csebianlander@gmail.com>
2021-07-08 11:12:23 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,13 +1,13 @@
 var mongoose = require("mongoose"),
     passportLocalMongoose = require("passport-local-mongoose");
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://chuck:tangabutts@cluster0.ywpal.mongodb.net/cobpe', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-});
+// mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://chuck:tangabutts@cluster0.ywpal.mongodb.net/cobpe', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true
+// });
 
 var userSchema = new mongoose.Schema({
   username: String,
